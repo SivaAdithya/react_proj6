001bbefb4a91406f81aa57aa600c1a1ddd0a0963
SivaAdithya <sivamj14@gmail.com>
2023-06-19 14:03:17 +0530

    first commit

--- src/comp/header.js
+++ src/comp/header.js
@@ -5,16 +5,16 @@ export function Header()
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link px-3" href="http://localhost:3000">Home</a>
+                        <a class="nav-link px-3" href="https://sivaadithya.github.io/react_proj6/">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link px-3" href="http://localhost:3000/about">About</a>
+                        <a class="nav-link px-3" href="https://sivaadithya.github.io/react_proj6/about">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link px-3" href="http://localhost:3000/project">Projects</a>
+                        <a class="nav-link px-3" href="https://sivaadithya.github.io/react_proj6//project">Projects</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link px-3" href="http://localhost:3000/contact">Contact</a>
+                        <a class="nav-link px-3" href="https://sivaadithya.github.io/react_proj6/contact">Contact</a>
                     </li>
                 </ul>
             </nav>
